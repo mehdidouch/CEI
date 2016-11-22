f44914e2799438042e35c9db2766e56234636ffc
Mehdi Douch <Mehdi.Douch@centralesupelec.fr>
2016-11-22 17:44:57 +0100

    deleted Fisher already in scoreDiff

--- Cox_script.m
+++ Cox_script.m
@@ -20,7 +20,11 @@ end
 t_censor = median(T);
 bool_censor = T>t_censor;
 
+% Value of beta given by fminsearch
 [beta_opt,fval] = fminsearch(@(x) -vraisemblance_cox(x,T,Z,t_censor),[0;0;0]);
+
+% We have implemented our own Newton-Raphson
+beta_opt_NR = NR_COX(T, Z, t_censor, [10;-2;3], 0.01);
 
 % Cox proportional hazards regression by Matlab
 b = coxphfit(Z,T,'censoring',bool_censor);
